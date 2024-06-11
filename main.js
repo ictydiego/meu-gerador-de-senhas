55ef2bec0185f1a31588030f3aa785fc80b9b81c
Diego Gonçalves de oliveira <97911163+ictydiego@users.noreply.github.com>
2024-06-11 10:14:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ let buttonElement = document.querySelector("#button");
 let tamanhoDaSenha = document.querySelector("#valor");
 let Senha = document.querySelector("#password");
 let containerPalavraChave = document.querySelector("#containerPalavraChave");
-
-
-
+//string com alfabeto maiusculo,minusculo,numeros e caracteres especiais.
 let letrasNumeCaracEsp = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!/!@#$%&*()_';
-
+//inicializa a variavel em vazio.
 let novaSenha = '';
-
+//atualiza o span tamanho senha, quando o usuario arrasta o slider
 tamanhoDaSenha.innerHTML = sliderElement.value;
 
 // Atualiza o valor do span ao mudar o input range
@@ -20,117 +18,16 @@ sliderElement.oninput = function() {
 
 function gerarSenha() {
   let SenhaGerada = '';
-  for(let i = 0, n = letrasNumeCaracEsp.length; i < sliderElement.value; ++i) {
-    SenhaGerada += letrasNumeCaracEsp.charAt(Math.floor(Math.random() * n));
+  for(let i = 0; i < sliderElement.value; ++i) {
+    SenhaGerada += letrasNumeCaracEsp.charAt(Math.floor(Math.random() *  letrasNumeCaracEsp.length));
   }
+  //remove a classe hide do container palavra chave para exibir na tela.
   containerPalavraChave.classList.remove("hide");
   Senha.innerHTML = SenhaGerada;
   novaSenha = SenhaGerada;
 }
-
+//ao clicar na sennha, é copiada a senha para area de transferencia
 function copyPassword() {
-  navigator.clipboard.writeText(novaSenha).then(() => {
-    alert('Senha copiada para a área de transferência!');
-  });
+  navigator.clipboard.writeText(novaSenha);
+  alert('Senha copiada para a área de transferência!');
 }
-
-const inputTarefa = document.querySelector('.input-tarefa');
-const btnTarefa = document.querySelector('.btn-add');
-const tarefas = document.querySelector('.senhas');
-const inputSenha = document.querySelector('.inputSenha');
-
-btnTarefa.addEventListener('click', () => {
-  if (!inputTarefa.value || !inputSenha.value) return alert("Todos os campos devem estar preenchidos");
-  criaTarefa(inputTarefa.value, inputSenha.value);
-});
-
-inputTarefa.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && inputTarefa.value && inputSenha.value) {
-    criaTarefa(inputTarefa.value, inputSenha.value);
-  }
-});
-
-function criaLi() {
-  return document.createElement('li');
-}
-
-function criaP() {
-  return document.createElement('p');
-}
-
-function limpaInput() {
-  inputTarefa.value = '';
-  inputSenha.value = '';
-  inputTarefa.focus();
-}
-
-function criaTarefa(textoInput, senhaInput) {
-  const li = criaLi();
-  const p = criaP();
-  p.innerText = senhaInput;
-  li.classList.add('senhasLi');
-  li.innerText = textoInput;
-  li.appendChild(p);
-  tarefas.appendChild(li);
-  limpaInput();
-  criaBotaoApagar(li);
-  salvarTarefas();
-}
-
-function criaBotaoApagar(li) {
-  const botaoApagar = document.createElement('button');
-  botaoApagar.innerText = 'Apagar';
-  botaoApagar.classList.add('Apagar');
-  li.appendChild(botaoApagar);
-}
-
-document.addEventListener('click', (e) => {
-  const el = e.target;
-  if (el.classList.contains('Apagar')) {
-    el.parentElement.remove();
-    salvarTarefas();
-  }
-});
-
-function salvarTarefas() {
-  const liTarefas = tarefas.querySelectorAll('li');
-  const listaDeTarefas = Array.from(liTarefas).map((tarefa) => ({
-    nome: tarefa.firstChild.nodeValue.trim(),
-    senha: tarefa.querySelector('p').innerText.trim()
-  }));
-  const tarefasJSON = JSON.stringify(listaDeTarefas);
-  localStorage.setItem('tarefas', tarefasJSON);
-}
-
-function adicionaTarefasSalvas() {
-  const tarefasJSON = localStorage.getItem('tarefas');
-  console.log(tarefasJSON);
-  const listaDeTarefas = JSON.parse(tarefasJSON) || [];
-  listaDeTarefas.forEach((tarefa) => criaTarefa(tarefa.nome, tarefa.senha));
-}
-
-//Exibir as senhas salvas atraves da senha fornecida.
-const containerExibir = document.querySelector('.container');
-const Exibir2 = document.querySelector('.exibir2');
-Exibir2.innerHTML = "Exibir Senhas";
-const spanCadeado = document.querySelector('.cadeado');
-const spanCadeado2 = document.querySelector('.cadeado2');
-function Exibir() {
-  if(containerExibir.classList.contains('hide')){
-    const number = prompt("Digite sua senha");
-    if(number === "1234"){
-      containerExibir.classList.toggle('hide');
-      spanCadeado2.classList.remove('hide');
-      spanCadeado.classList.add('hide');
-      Exibir2.innerHTML = "Ocultar Senhas";
-    }  
-  }else{
-    containerExibir.classList.toggle('hide');
-    spanCadeado2.classList.add('hide');
-    spanCadeado.classList.remove('hide');
-    Exibir2.innerHTML = "Exibir Senhas";
-    }
-  }
-
-
-adicionaTarefasSalvas();
